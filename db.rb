7d77d329e8a2d5709bbfd5e7244a84ccdb4323cb
Manjilan <nakarmimanjila@yahoo.com>
2018-02-16 14:41:16 -0800

    populated the database

--- db.rb
+++ db.rb
@@ -6,3 +6,19 @@
 # - CRUD operations (CREATE, SELECT, UPDATE, DELETE)
 #
 require 'pg'
+
+def connection
+  PG::Connection.new(dbname: 'trivia')
+end
+
+def add_table(questions)
+  connection.exec("CREATE TABLE #{questions} (id SERIAL PRIMARY KEY, question VARCHAR(255), answer VARCHAR(255))")
+  p "Table created: #{questions}"
+
+end
+
+def add_questions(question, answer)
+   connection.exec("INSERT INTO questions (question, answer) VALUES ('#{question}','#{answer}')")
+ #include the list of values you want to add as arguments or add id manually
+   p "User created: #{question}, #{answer} "
+end
