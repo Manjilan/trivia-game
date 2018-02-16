d4d7dc1fcb8af10a971e2facb90480fc0b6da9ac
Manjilan <nakarmimanjila@yahoo.com>
2018-02-16 13:46:56 -0800

    added the models

---
+++ db.rb
@@ -0,0 +1,8 @@
+# Database Configuration
+# ======================
+# Documentation: https://deveiate.org/code/pg/PG.html
+# Use this module to:
+# - Connect to the adventure database
+# - CRUD operations (CREATE, SELECT, UPDATE, DELETE)
+#
+require 'pg'
