2c76065dac39cbe552e17648c735539eecccf74f
xingzo <kingsleynyaosi@gmail.com>
2018-02-16 14:41:38 -0800

    - made a validate_answer function (this should go into the db.rb file) - completed ask_question function
    This was done by Felix.

--- trivia.rb
+++ trivia.rb
@@ -18,6 +18,24 @@ class Game
       puts "id is less than 5"
       # since id is less than 5 we are going to display the next question
       display_question(id)
+      user_input = gets.chomp.to_sym
+
+      validate = validate_answer(user_input)
+
+      if validate = true
+        id++
+        ask_question(id)
+      end
+
+      else
+        puts "You are wrong."
+        return
+      end
+    end
+
+    else
+      puts "You win! Yay!"
+      return
     end
   end
 
@@ -26,16 +44,33 @@ class Game
   def display_question(id)
 
     puts "we are in the display question function"
-  #   connection.exec( "SELECT * FROM books" ) do |result|
-  # puts "ID | Title"
-  # result.each do |row|
-  #   puts " %s | %s" % row.values_at('id', 'title')
+  #   connection.exec( "SELECT question FROM questions WHERE id = #{id}" ) do |result|
+  #     puts "ID | Title"
+  #     result.each do |row|
+  #       puts " %s | %s" % row.values_at('id', 'question')
   # end
   # end
 
 
   end
+def validate_answer(id, user_input)
+  connection.exec( "SELECT answer FROM questions WHERE id = #{id}" ) do |result|
+      puts "ID | Title"
+      result.each do |row|
+        actual_answer = " %s " % row.values_at('answer')
 
+        if user_input == actual_answer
+            puts "Answer is correct!"
+            return true
+        end
+
+      else
+        return false
+      end
+  end
+  end
+
+end
 
 
 end
